0e8f65eaea5a64aa7b8dc5a7c0bf6bb2875a4dd9
Lewis-v <www.605788229@qq.com>
2018-01-27 12:35:16 +0800

    完善个人消息置顶

--- src/api/TopPut.java
+++ src/api/TopPut.java
@@ -11,6 +11,7 @@ import net.sf.json.JSONObject;
 import utils.ServiceResult;
 
 public class TopPut extends BaseApi{
+	private boolean isSuccess = false;//是否成功
 
 	public TopPut(Map<String, String> parmMap) {
 		super(parmMap);
@@ -19,21 +20,33 @@ public class TopPut extends BaseApi{
 	@Override
 	public FullHttpResponse getResponse() throws IOException {
 		if (parmMap.containsKey("id") && parmMap.containsKey("act_id")
-				&& parmMap.containsKey("type") && parmMap.containsKey("top_num")){
-			List<JSONObject> list = SqlConnection.getInstance().search("*", "user_id = "+parmMap.get("id")+
-					" AND top_num = "+parmMap.get("top_num"), "top");
+				&& parmMap.containsKey("type")){
+
+			List<JSONObject> list = SqlConnection.getInstance().search("*", "user_id = "+parmMap.get("id")+"  Order by top_num ", "top");
 			if (list != null && list.size()>0){
-				if(SqlConnection.getInstance().setData("act_id,type", parmMap.get("act_id")+","+parmMap.get("type")
-				,"user_id = "+parmMap.get("id")+" AND top_num = "+parmMap.get("top_num") , "top")){
+				if (list.size()>=2 && parmMap.containsKey("top_num") && parmMap.containsKey("top_num2")){
+					SqlConnection.getInstance().setData("top_num", parmMap.get("top_num"), "user_id = "+parmMap.get("id")+" AND top_num = "+parmMap.get("top_num2"), "top");
+					SqlConnection.getInstance().setData("top_num",  parmMap.get("top_num"), "user_id = "+parmMap.get("id")
+					+" AND type = "+parmMap.get("type") + " AND act_id = "+parmMap.get("act_id"), "top");
 					setStatus(SUCCESS);
 					setMessage("操作成功");
+
 				}else{
-					setStatus(FAIL);
-					setMessage("操作失败");
+					if (list.size() == 3){//删除最后一个
+						if (!SqlConnection.getInstance().removeData("user_id = "+parmMap.get("id") +" AND top_num = 3", "top")){
+
+						}
+						list.remove(2);
+					}
+					for (int num = 0,len = list.size();num < len;num ++){
+						SqlConnection.getInstance().setData("top_num", String.valueOf(num+2), "user_id = "+parmMap.get("id")+" AND top_num = "+num+1, "top");
+					}
+					SqlConnection.getInstance().insertData("user_id,act_id,type,top_num", parmMap.get("id")+","+
+							parmMap.get("act_id")+","+parmMap.get("type")+",1", "top");
 				}
 			}else{
 				if (SqlConnection.getInstance().insertData("user_id,act_id,type,top_num", parmMap.get("id")+","+
-						parmMap.get("act_id")+","+parmMap.get("type")+","+parmMap.get("top_num"), "top")){
+						parmMap.get("act_id")+","+parmMap.get("type")+",1", "top")){
 					setStatus(SUCCESS);
 					setMessage("操作成功");
 				}else{
