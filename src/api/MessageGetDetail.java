13990fd7b5f8e5bc4cf6a21ebfe07f12620263c9
Lewis-v <www.605788229@qq.com>
2018-02-10 23:56:38 +0800

    完善消息模块

--- src/api/MessageGetDetail.java
+++ src/api/MessageGetDetail.java
@@ -21,19 +21,25 @@ public class MessageGetDetail extends BaseApi{
 		if (parmMap.containsKey("put_id") && parmMap.containsKey("get_id")){
 			String put_id = parmMap.get("put_id");
 			String get_id = parmMap.get("get_id");
-			String where = "(put_id = " + put_id + " AND get_id = " + get_id 
-					+") OR (get_id = " + put_id + " AND put_id = "+get_id+")";
-			List<JSONObject> list = SqlConnection.getInstance().search("*", where, "message_detail");
-			addLog(SqlConnection.getInstance().getLog());
-			if (list != null && list.size()>0){
-				setStatus(SUCCESS);
-				setMessage("success");
-				setData(list.toString());
-				addLog(list.toString());
-			}else{
-				setStatus(FAIL);
-				setMessage("获取失败");
+			String from = "message_detail";
+			String where = "((put_id = " + put_id + " AND get_id = " + get_id 
+					+") OR (get_id = " + put_id + " AND put_id = "+get_id+"))";
+			if(parmMap.containsKey("type") && parmMap.containsKey("start_id")){
+				if (parmMap.get("type").equals("new")){//加载新消息
+					where = where + " AND id > "+parmMap.get("start_id");
+				}else if(parmMap.get("type").equals("history")){//加载历史消息
+					where = where + " AND id < "+parmMap.get("start_id");
+					from = from + "LIMIT 20";
+				}
+			}else {//默认只会返回最近的100条,其他的消息系统会定期清理
+				from = from + "LIMIT 100";
 			}
+			List<JSONObject> list = SqlConnection.getInstance().search("*", where, from);
+			addLog(SqlConnection.getInstance().getLog());
+			setStatus(SUCCESS);
+			setMessage("success");
+			setData(list.toString());
+			addLog(list.toString());
 		}else {
 			setStatus(DATA_FAIL);
 			setMessage("访问失败");
